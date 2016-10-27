e18fdd0dbfb678b735fc8a02b9ad8eae4e4adbd4
andrey <andrey@cossacklabs.com>
2016-10-27 13:50:15 +0300

    Add tests

--- src/rd_themis.c
+++ src/rd_themis.c
@@ -23,7 +23,6 @@ static int cmd_scell_seal_encrypt(RedisModuleCtx *ctx, RedisModuleString **argv,
       return REDISMODULE_ERR;      
     }
     themis_status_t res = themis_secure_cell_encrypt_seal((const uint8_t*)pass, pass_len, NULL, 0, (const uint8_t*)message, message_len, encrypted_data, &encrypted_data_len);
-    fprintf(stderr, "%i\n", res);
     if(THEMIS_SUCCESS!=res){
       free(encrypted_data);
       RedisModule_ReplyWithError(ctx, "ERR secure seal encription failed");
@@ -75,6 +74,117 @@ static int cmd_scell_seal_decrypt(RedisModuleCtx *ctx, RedisModuleString **argv,
     return REDISMODULE_OK;
 }
 
+//encrypt data with acra ctruct 
+static int smessage_encrypt(const uint8_t* data, const uint32_t data_length, const uint8_t* private_key, const uint32_t private_key_length, const uint8_t* public_key, const uint32_t public_key_length, const uint8_t* peer_public_key, const uint32_t peer_public_key_length, uint8_t** enc_data, uint32_t* enc_data_length){
+  if(THEMIS_BUFFER_TOO_SMALL!=themis_secure_message_wrap(private_key, private_key_length, peer_public_key, peer_public_key_length, data, data_length, NULL, (size_t*)enc_data_length)){
+    return -1;
+  }
+  *enc_data = malloc(public_key_length+sizeof(uint32_t)+*enc_data_length);
+  if(!(*enc_data)){
+    return -2;
+  }
+  memcpy(*enc_data, &public_key_length, sizeof(public_key_length));
+  memcpy(*enc_data+sizeof(public_key_length), public_key, public_key_length);
+  if(THEMIS_SUCCESS!=themis_secure_message_wrap(private_key, private_key_length, peer_public_key, peer_public_key_length, data, data_length, *enc_data+sizeof(public_key_length)+public_key_length, (size_t*)enc_data_length)){
+    free(*enc_data);
+    return -1;
+  }
+  *enc_data_length+=sizeof(public_key_length)+public_key_length;
+  return 0;
+}
+
+//decrypt  acra structed data 
+static int smessage_decrypt(const uint8_t* data, const uint32_t data_length, const uint8_t* private_key, const uint32_t private_key_length, uint8_t** dec_data, uint32_t* dec_data_length){
+  const uint32_t public_key_length = *((const uint32_t*)data);
+  if(data_length<=public_key_length+sizeof(uint32_t)){
+    return -1;
+  }
+  const uint8_t* public_key = data+sizeof(uint32_t);
+  const uint8_t* data_ = public_key+public_key_length;
+  size_t data_length_ = data_length-public_key_length-sizeof(uint32_t); 
+  
+  if(THEMIS_BUFFER_TOO_SMALL!=themis_secure_message_unwrap(private_key, private_key_length, public_key, public_key_length, data_, data_length_, NULL, (size_t*)dec_data_length)){
+    return -1;
+  }
+  *dec_data = malloc(*dec_data_length);
+  if(!(*dec_data)){
+    return -2;
+  }
+  if(THEMIS_SUCCESS!=themis_secure_message_unwrap(private_key, private_key_length, public_key, public_key_length, data_, data_length_, *dec_data, (size_t*)dec_data_length)){
+    free(*dec_data);
+    return -1;
+  }
+  return 0;
+}
+
+static int smessage_enc(const uint8_t* public_key, const uint32_t public_key_length, const uint8_t* data, const uint8_t data_length, uint8_t** enc_data, uint32_t* enc_data_length){
+  uint8_t new_private_key[10240];
+  uint8_t new_public_key[10240];
+
+  size_t new_private_key_length=10240, new_public_key_length=10240;
+  if(THEMIS_SUCCESS!=themis_gen_ec_key_pair(new_private_key, &new_private_key_length, new_public_key, &new_public_key_length)){
+    return -1;
+  }
+  return smessage_encrypt(data, data_length, new_private_key, new_private_key_length, new_public_key, new_public_key_length, public_key, public_key_length, enc_data, enc_data_length);
+}
+
+static int smessage_dec(const uint8_t* private_key, const uint32_t private_key_length, const uint8_t* data, const uint8_t data_length, uint8_t** dec_data, uint32_t* dec_data_length){
+  return smessage_decrypt(data, data_length, private_key, private_key_length, dec_data, dec_data_length);
+}
+
+static int cmd_smessage_encrypt(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+    RedisModuleCallReply *reply;
+    if (argc != 4) {
+        RedisModule_WrongArity(ctx);
+        return REDISMODULE_OK;
+    }
+
+    size_t encrypted_data_len=0, public_key_len=0, message_len=0;
+    const char *public_key = RedisModule_StringPtrLen(argv[3], &public_key_len);
+    const char *message = RedisModule_StringPtrLen(argv[2], &message_len);
+    uint8_t *encrypted_data=NULL;
+    if(0 != smessage_enc((const uint8_t*)public_key, public_key_len, (const uint8_t*)message, message_len, &encrypted_data, (uint32_t*)&encrypted_data_len)){
+      RedisModule_ReplyWithError(ctx, "ERR secure message encription failed");
+      return REDISMODULE_ERR;      
+    }
+    reply = RedisModule_Call(ctx, "SET", "sb!", argv[1], encrypted_data, encrypted_data_len);
+    RedisModule_ReplyWithCallReply(ctx, reply);
+    free(encrypted_data);
+    return REDISMODULE_OK;
+}
+
+static int cmd_smessage_decrypt(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+    RedisModuleCallReply *reply;
+    if (argc != 3) {
+        RedisModule_WrongArity(ctx);
+        return REDISMODULE_OK;
+    }
+
+    reply = RedisModule_Call(ctx, "GET", "s", argv[1]);
+    if(REDISMODULE_REPLY_NULL == RedisModule_CallReplyType(reply)){
+      RedisModule_ReplyWithLongLong(ctx, 0);
+      return REDISMODULE_OK;
+    }
+
+    if (REDISMODULE_REPLY_STRING != RedisModule_CallReplyType(reply)) {
+      RedisModule_ReplyWithError(ctx, "WRONGTYPE Operation against a key holding the wrong kind of value");
+      return REDISMODULE_ERR;      
+    }
+
+    size_t decrypted_data_len=0, private_key_len=0, message_len=0;
+    const char *private_key = RedisModule_StringPtrLen(argv[2], &private_key_len);
+    const char *message = RedisModule_CallReplyStringPtr(reply, &message_len);
+    uint8_t *decrypted_data = NULL;    
+    if(0 != smessage_dec((const uint8_t*)private_key, private_key_len, (const uint8_t*)message, message_len, &decrypted_data, (uint32_t*)&decrypted_data_len)){
+      RedisModule_ReplyWithError(ctx, "ERR secure message encription failed");
+      return REDISMODULE_ERR;      
+    }
+    RedisModule_ReplyWithStringBuffer(ctx, (const char*)decrypted_data, decrypted_data_len);
+    free(decrypted_data);
+    return REDISMODULE_OK;
+}
+
+
 int RedisModule_OnLoad(RedisModuleCtx *ctx) {
     if (RedisModule_Init(ctx, "themis", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
@@ -82,5 +192,9 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx) {
         return REDISMODULE_ERR;
     if (RedisModule_CreateCommand(ctx, "themis.scell_seal_get", cmd_scell_seal_decrypt, "no-monitor fast", 1, 1, 1) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
+    if (RedisModule_CreateCommand(ctx, "themis.smessage_set", cmd_smessage_encrypt, "no-monitor fast", 1, 1, 1) == REDISMODULE_ERR)
+      return REDISMODULE_ERR;
+    if (RedisModule_CreateCommand(ctx, "themis.smessage_get", cmd_smessage_decrypt, "no-monitor fast", 1, 1, 1) == REDISMODULE_ERR)
+      return REDISMODULE_ERR;
     return REDISMODULE_OK;
 }
--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/shunit2
+#!/bin/bash
 
 testRedisAlive() {
     res=`redis-cli ping`
@@ -7,7 +7,7 @@ testRedisAlive() {
 
 testLoadModule() {
     curdir=`pwd`
-    res=`redis-cli module load ${curdir}/themis_module.so`
+    res=`redis-cli module load ${curdir}/rd_themis.so`
     assertEquals "OK" "$res"
 }
 
@@ -21,8 +21,21 @@ testThemisSealGet() {
     assertEquals "test_data" "$res"
 }
 
+testThemisMessageSet() {
+    res=`echo -ne '\x55\x45\x43\x32\x00\x00\x00\x2d\x6b\xbb\x79\x79\x03\xfa\xb7\x33\x3a\x4d\x6e\xb7\xc2\x59\xde\x78\x96\xfa\x69\xe6\x63\x86\x91\xc2\x65\xa0\x92\xf6\x5a\x22\x3c\xa9\x8e\xc9\xa7\x35\x42' | redis-cli -x themis.smessage_set test_key test_data`
+    assertEquals "OK" "$res"
+}
+
+testThemisMessageGet() {
+    res=`echo -ne '\x52\x45\x43\x32\x00\x00\x00\x2d\xc7\xa8\xca\x7a\x00\xc3\xb5\xd1\xad\x51\x37\x30\x8f\x45\xe6\x5e\x54\xdf\x2b\x7a\x45\xbc\x85\x08\xe8\xcc\x3b\xc9\x48\x1b\x63\x1a\xe8\x12\x8b\x39\x74' | redis-cli -x themis.smessage_get test_key `
+    assertEquals "test_data" "$res"
+}
+
+
 testUnloadModule() {
     curdir=`pwd`
     res=`redis-cli module unload themis`
     assertEquals "OK" "$res"
 }
+
+. shunit2
