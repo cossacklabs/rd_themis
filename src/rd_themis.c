489ae8dbb290f2dcc308c6a5f939469cdfcb6caf
andrey <andrey@cossacklabs.com>
2016-11-20 19:59:19 +0200

    rewrite module to use low level redis module API

--- src/rd_themis.c
+++ src/rd_themis.c
@@ -1,3 +1,4 @@
+
 /*
 * Copyright (c) 2016 Cossack Labs Limited
 *
@@ -20,7 +21,6 @@
 #include <themis/themis.h>
 
 static int cmd_scell_seal_encrypt(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-    RedisModuleCallReply *reply;
     if (argc != 4) {
         RedisModule_WrongArity(ctx);
         return REDISMODULE_ERR;
@@ -33,79 +33,90 @@ static int cmd_scell_seal_encrypt(RedisModuleCtx *ctx, RedisModuleString **argv,
       RedisModule_ReplyWithError(ctx, "ERR secure seal encryption failed (length determination)");
       return REDISMODULE_ERR;      
     }
-    uint8_t *encrypted_data = malloc(encrypted_data_len);
-    if(!encrypted_data){
-      RedisModule_ReplyWithError(ctx, "ERR memory allocation error");
-      return REDISMODULE_ERR;      
+    RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1], REDISMODULE_WRITE);
+    if(REDISMODULE_OK != RedisModule_StringTruncate(key, encrypted_data_len)){
+      RedisModule_DeleteKey(key);
+      RedisModule_CloseKey(key);
+      RedisModule_ReplyWithError(ctx, "ERR redis string truncate error");
+      return REDISMODULE_ERR;            
     }
+    uint8_t* encrypted_data = (uint8_t*)(RedisModule_StringDMA(key, &encrypted_data_len, REDISMODULE_WRITE));
     themis_status_t res = themis_secure_cell_encrypt_seal((const uint8_t*)pass, pass_len, NULL, 0, (const uint8_t*)message, message_len, encrypted_data, &encrypted_data_len);
     if(THEMIS_SUCCESS!=res){
-      free(encrypted_data);
+      RedisModule_DeleteKey(key);
+      RedisModule_CloseKey(key);
       RedisModule_ReplyWithError(ctx, "ERR secure seal encryption failed");
       return REDISMODULE_ERR;      
     }
-    reply = RedisModule_Call(ctx, "SET", "sb!", argv[1], encrypted_data, encrypted_data_len);
-    RedisModule_ReplyWithCallReply(ctx, reply);
-    free(encrypted_data);
+    RedisModule_CloseKey(key);
+    RedisModule_ReplyWithSimpleString(ctx, "OK");
     return REDISMODULE_OK;
 }
 
 static int cmd_scell_seal_decrypt(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-    RedisModuleCallReply *reply;
     if (argc != 3) {
         RedisModule_WrongArity(ctx);
         return REDISMODULE_OK;
     }
 
-    reply = RedisModule_Call(ctx, "GET", "s", argv[1]);
-    if(REDISMODULE_REPLY_NULL == RedisModule_CallReplyType(reply)){
+    RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ);
+    if(NULL == key){
       RedisModule_ReplyWithLongLong(ctx, 0);
       return REDISMODULE_OK;
     }
 
-    if (REDISMODULE_REPLY_STRING != RedisModule_CallReplyType(reply)) {
+    if (REDISMODULE_KEYTYPE_STRING != RedisModule_KeyType(key)) {
+      RedisModule_CloseKey(key);
       RedisModule_ReplyWithError(ctx, REDISMODULE_ERRORMSG_WRONGTYPE);
       return REDISMODULE_ERR;      
     }
 
     size_t decrypted_data_len=0, pass_len=0, message_len=0;
-    const char *pass = RedisModule_StringPtrLen(argv[2], &pass_len);
-    const char *message = RedisModule_CallReplyStringPtr(reply, &message_len);
-    if(THEMIS_BUFFER_TOO_SMALL!=themis_secure_cell_decrypt_seal((const uint8_t*)pass, pass_len, NULL, 0, (const uint8_t*)message, message_len, NULL, &decrypted_data_len)){
+    const uint8_t *message=(const uint8_t*)(RedisModule_StringDMA(key, &message_len, REDISMODULE_READ));
+    const uint8_t *pass = (const uint8_t*)(RedisModule_StringPtrLen(argv[2], &pass_len));
+    if(THEMIS_BUFFER_TOO_SMALL!=themis_secure_cell_decrypt_seal(pass, pass_len, NULL, 0, message, message_len, NULL, &decrypted_data_len)){
+      RedisModule_CloseKey(key);
       RedisModule_ReplyWithError(ctx, "ERR secure seal decryption failed (length determination)");
       return REDISMODULE_ERR;      
     }
     uint8_t *decrypted_data = malloc(decrypted_data_len);
     if(!decrypted_data){
+      RedisModule_CloseKey(key);      
       RedisModule_ReplyWithError(ctx, "ERR memory allocation error");
       return REDISMODULE_ERR;      
     }
-    if(THEMIS_SUCCESS!=themis_secure_cell_decrypt_seal((const uint8_t*)pass, pass_len, NULL, 0, (const uint8_t*)message, message_len, decrypted_data, &decrypted_data_len)){
+    if(THEMIS_SUCCESS!=themis_secure_cell_decrypt_seal(pass, pass_len, NULL, 0, message, message_len, decrypted_data, &decrypted_data_len)){
+      RedisModule_CloseKey(key);      
       free(decrypted_data);
       RedisModule_ReplyWithError(ctx, "ERR secure seal decryption failed");
       return REDISMODULE_ERR;      
     }
+    RedisModule_CloseKey(key);      
     RedisModule_ReplyWithStringBuffer(ctx, (const char*)decrypted_data, decrypted_data_len);
     free(decrypted_data);
     return REDISMODULE_OK;
 }
 
-//encrypt data with acra ctruct 
-static int smessage_encrypt(const uint8_t* data, const uint32_t data_length, const uint8_t* private_key, const uint32_t private_key_length, const uint8_t* public_key, const uint32_t public_key_length, const uint8_t* peer_public_key, const uint32_t peer_public_key_length, uint8_t** enc_data, uint32_t* enc_data_length){
-  if(THEMIS_BUFFER_TOO_SMALL!=themis_secure_message_wrap(private_key, private_key_length, peer_public_key, peer_public_key_length, data, data_length, NULL, (size_t*)enc_data_length)){
-    return -1;
+static size_t smessage_encrypt_len(const uint8_t* data, const uint32_t data_length, const uint8_t* private_key, const uint32_t private_key_length, const uint8_t* public_key, const uint32_t public_key_length, const uint8_t* peer_public_key, const uint32_t peer_public_key_length){
+  size_t enc_len=0;
+  if(THEMIS_BUFFER_TOO_SMALL!=themis_secure_message_wrap(private_key, private_key_length, peer_public_key, peer_public_key_length, data, data_length, NULL, &enc_len)){
+    return 0;
   }
-  *enc_data = malloc(public_key_length+sizeof(uint32_t)+*enc_data_length);
-  if(!(*enc_data)){
+  return enc_len+sizeof(public_key_length)+public_key_length;
+}
+
+//encrypt data with acra ctruct 
+static int smessage_encrypt(const uint8_t* data, const uint32_t data_length, const uint8_t* private_key, const uint32_t private_key_length, const uint8_t* public_key, const uint32_t public_key_length, const uint8_t* peer_public_key, const uint32_t peer_public_key_length, uint8_t* enc_data, uint32_t *enc_data_length){
+  if(*enc_data_length<sizeof(public_key_length)+public_key_length){
     return -2;
   }
-  memcpy(*enc_data, &public_key_length, sizeof(public_key_length));
-  memcpy(*enc_data+sizeof(public_key_length), public_key, public_key_length);
-  if(THEMIS_SUCCESS!=themis_secure_message_wrap(private_key, private_key_length, peer_public_key, peer_public_key_length, data, data_length, *enc_data+sizeof(public_key_length)+public_key_length, (size_t*)enc_data_length)){
-    free(*enc_data);
-    return -1;
+  memcpy(enc_data, &public_key_length, sizeof(public_key_length));
+  memcpy(enc_data+sizeof(public_key_length), public_key, public_key_length);
+  size_t enc_len=*enc_data_length-sizeof(public_key_length)-public_key_length;
+  if(THEMIS_SUCCESS!=themis_secure_message_wrap(private_key, private_key_length, peer_public_key, peer_public_key_length, data, data_length, enc_data+sizeof(public_key_length)+public_key_length, &enc_len)){
+    return -2;
   }
-  *enc_data_length+=sizeof(public_key_length)+public_key_length;
+  *enc_data_length = enc_len+sizeof(public_key_length)+public_key_length;
   return 0;
 }
 
@@ -133,68 +144,79 @@ static int smessage_decrypt(const uint8_t* data, const uint32_t data_length, con
   return 0;
 }
 
-static int smessage_enc(const uint8_t* public_key, const uint32_t public_key_length, const uint8_t* data, const uint8_t data_length, uint8_t** enc_data, uint32_t* enc_data_length){
-  uint8_t new_private_key[10240];
-  uint8_t new_public_key[10240];
-
-  size_t new_private_key_length=10240, new_public_key_length=10240;
-  if(THEMIS_SUCCESS!=themis_gen_ec_key_pair(new_private_key, &new_private_key_length, new_public_key, &new_public_key_length)){
-    return -1;
-  }
-  return smessage_encrypt(data, data_length, new_private_key, new_private_key_length, new_public_key, new_public_key_length, public_key, public_key_length, enc_data, enc_data_length);
-}
-
 static int smessage_dec(const uint8_t* private_key, const uint32_t private_key_length, const uint8_t* data, const uint8_t data_length, uint8_t** dec_data, uint32_t* dec_data_length){
   return smessage_decrypt(data, data_length, private_key, private_key_length, dec_data, dec_data_length);
 }
 
 static int cmd_smessage_encrypt(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-    RedisModuleCallReply *reply;
     if (argc != 4) {
         RedisModule_WrongArity(ctx);
         return REDISMODULE_OK;
     }
 
-    size_t encrypted_data_len=0, public_key_len=0, message_len=0;
+    uint8_t new_private_key[10240];
+    uint8_t new_public_key[10240];
+    size_t new_private_key_length=10240, new_public_key_length=10240;
+    if(THEMIS_SUCCESS!=themis_gen_ec_key_pair(new_private_key, &new_private_key_length, new_public_key, &new_public_key_length)){
+      RedisModule_ReplyWithError(ctx, "ERR key pair generation error");
+      return REDISMODULE_ERR;      
+    }
+    size_t public_key_len=0, message_len=0;
     const char *public_key = RedisModule_StringPtrLen(argv[2], &public_key_len);
     const char *message = RedisModule_StringPtrLen(argv[3], &message_len);
-    uint8_t *encrypted_data=NULL;
-    if(0 != smessage_enc((const uint8_t*)public_key, public_key_len, (const uint8_t*)message, message_len, &encrypted_data, (uint32_t*)&encrypted_data_len)){
+    size_t encrypted_data_len = smessage_encrypt_len((const uint8_t*)message, message_len, new_private_key, new_private_key_length,  new_public_key, new_public_key_length, (const uint8_t*)public_key, public_key_len);
+    if(0==encrypted_data_len){
+      RedisModule_ReplyWithError(ctx, "ERR secure message encryption failed (length determination)");
+      return REDISMODULE_ERR;            
+    }
+    RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1], REDISMODULE_WRITE);
+    if(REDISMODULE_OK != RedisModule_StringTruncate(key, encrypted_data_len)){
+      RedisModule_DeleteKey(key);
+      RedisModule_CloseKey(key);
+      RedisModule_ReplyWithError(ctx, "ERR redis string truncate error");
+      return REDISMODULE_ERR;            
+    }
+    uint8_t* encrypted_data = (uint8_t*)(RedisModule_StringDMA(key, &encrypted_data_len, REDISMODULE_WRITE));
+
+    if(0 != smessage_encrypt((const uint8_t*)message, message_len, new_private_key, new_private_key_length, new_public_key, new_public_key_length, (const uint8_t*)public_key, public_key_len, encrypted_data, (uint32_t*)(&encrypted_data_len))){
+      RedisModule_DeleteKey(key);
+      RedisModule_CloseKey(key);
       RedisModule_ReplyWithError(ctx, "ERR secure message encryption failed");
       return REDISMODULE_ERR;      
     }
-    reply = RedisModule_Call(ctx, "SET", "sb!", argv[1], encrypted_data, encrypted_data_len);
-    RedisModule_ReplyWithCallReply(ctx, reply);
-    free(encrypted_data);
+    RedisModule_CloseKey(key);
+    RedisModule_ReplyWithSimpleString(ctx, "OK");
     return REDISMODULE_OK;
 }
 
 static int cmd_smessage_decrypt(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-    RedisModuleCallReply *reply;
     if (argc != 3) {
         RedisModule_WrongArity(ctx);
         return REDISMODULE_OK;
     }
 
-    reply = RedisModule_Call(ctx, "GET", "s", argv[1]);
-    if(REDISMODULE_REPLY_NULL == RedisModule_CallReplyType(reply)){
+    RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ);
+    if(NULL == key){
       RedisModule_ReplyWithLongLong(ctx, 0);
       return REDISMODULE_OK;
     }
 
-    if (REDISMODULE_REPLY_STRING != RedisModule_CallReplyType(reply)) {
+    if (REDISMODULE_KEYTYPE_STRING != RedisModule_KeyType(key)) {
+      RedisModule_CloseKey(key);
       RedisModule_ReplyWithError(ctx, REDISMODULE_ERRORMSG_WRONGTYPE);
       return REDISMODULE_ERR;      
     }
 
     size_t decrypted_data_len=0, private_key_len=0, message_len=0;
-    const char *private_key = RedisModule_StringPtrLen(argv[2], &private_key_len);
-    const char *message = RedisModule_CallReplyStringPtr(reply, &message_len);
+    const uint8_t *private_key = (const uint8_t*)(RedisModule_StringPtrLen(argv[2], &private_key_len));
+    const uint8_t *message=(const uint8_t*)(RedisModule_StringDMA(key, &message_len, REDISMODULE_READ));
     uint8_t *decrypted_data = NULL;    
-    if(0 != smessage_dec((const uint8_t*)private_key, private_key_len, (const uint8_t*)message, message_len, &decrypted_data, (uint32_t*)&decrypted_data_len)){
+    if(0 != smessage_dec(private_key, private_key_len, message, message_len, &decrypted_data, (uint32_t*)&decrypted_data_len)){
+      RedisModule_CloseKey(key);
       RedisModule_ReplyWithError(ctx, "ERR secure message encryption failed");
       return REDISMODULE_ERR;      
     }
+    RedisModule_CloseKey(key);
     RedisModule_ReplyWithStringBuffer(ctx, (const char*)decrypted_data, decrypted_data_len);
     free(decrypted_data);
     return REDISMODULE_OK;
